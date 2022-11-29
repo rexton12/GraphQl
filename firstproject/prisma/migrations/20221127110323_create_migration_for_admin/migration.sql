f0f36239993c1db40579919ef0e46d69a7a238b3
rexton12 <117345917+rexton12@users.noreply.github.com>
2022-11-29 01:12:49 -0800

    updated

--- firstproject/prisma/migrations/20221127110323_create_migration_for_admin/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "admin" (
-    "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "desc" TEXT,
-
-    CONSTRAINT "admin_pkey" PRIMARY KEY ("id")
-);
